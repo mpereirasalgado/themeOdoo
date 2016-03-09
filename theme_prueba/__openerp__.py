7e6b56da0a69493f546cb7d0409e8d614e6600eb
Micael <micael11rm@ourlook.com>
2016-03-09 08:51:54 +0100

    subir a github

---
+++ theme_prueba/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Tema prueba',
+    'description': 'tema de prueba',
+    'version': '1.0',
+    'author': 'Mica.S.S',
+    
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/assets.xml'
+        
+    ],
+    'category': 'Theme/Hidden',
+    'depends': ['website'],
+
+    'application': False,
+}
